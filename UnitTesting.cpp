62dc306767e81b51a002a52d9af9224fa0fb9adb
tyleraharrison <tyleraharrison@gmail.com>
2020-03-04 17:56:11 -0600

    Added ArrayBet and some Unit Tests

--- UnitTesting.cpp
+++ UnitTesting.cpp
@@ -8,7 +8,7 @@ int main(){
     cout << setprecision(2) << fixed;
 
     double list[] = {100.0, 200.0, 300.0};
-    for (auto num : list) //Line 2
+    for (auto num : list)
         cout << num << " ";
 
     cout << endl << strlen("Tyler") << endl;
