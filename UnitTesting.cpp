fcfd6b7b88279602b568dfb13989ca30bce00ad9
tyleraharrison <tyleraharrison@gmail.com>
2020-03-03 23:41:47 -0600

    Unit tested for Chapter 8 before bed

---
+++ UnitTesting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+using namespace std;
+
+int main(){
+
+    cout << setprecision(2) << fixed;
+
+    double list[] = {100.0, 200.0, 300.0};
+    for (auto num : list) //Line 2
+        cout << num << " ";
+
+    cout << endl << strlen("Tyler") << endl;
+
+}
