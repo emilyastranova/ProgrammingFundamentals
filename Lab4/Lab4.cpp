43539c0c53da168e0b14552836b7d0ba676f1f85
tyleraharrison <tyleraharrison@gmail.com>
2020-04-14 17:15:03 -0500

    Updated template and half lab4
    Added space functions

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -3,7 +3,36 @@
 #include <cmath>
 using namespace std;
 
-int main(){
+string space() // I got tired of typing three tabs
+{
+    return "\t\t\t";
+}
+string space(int x) // Specified amount of tabs (e.g. space(4))
+{
+    string tab = "";
 
+    for (int i = 0; i < x; i++)
+        tab = tab + "\t";
 
+    return tab;
+}
+
+int main()
+{
+    char more;
+    double x;
+
+    do // Input loop, ends when user says N
+    {
+        cout << "\n\t\t\t\tInput X: ";
+        cin >> x;
+
+        cout << setw(10) << "\n" << space(4) << "       LibraryResult\tMyResult" << endl;
+        cout << space() << setprecision(2) << fixed << "sin< " << x << ">" << "\t" << setw(10) << setprecision(6) << fixed << sin(x) << endl;
+        cout << space() << setprecision(2) << fixed << "cos< " << x << ">" << "\t" << setw(10) << setprecision(6) << fixed << cos(x) << endl;
+        cout << space() << setprecision(2) << fixed << "exp< " << x << ">" << "\t" << setw(10) << setprecision(6) << fixed << exp(x) << endl;
+
+        cout << "\n\t\t\tDo more (Y/N)? ";
+        cin >> more;
+    } while (more == 'y' || more == 'Y');
 }
--- Template.cpp
+++ Template.cpp
@@ -3,6 +3,20 @@
 #include <cmath>
 using namespace std;
 
+string space() // I got tired of typing three tabs
+{
+    return "\t\t\t";
+}
+string space(int x) // Specified amount of tabs (e.g. space(4))
+{
+    string tab = "";
+
+    for (int i = 0; i < x; i++)
+        tab = tab + "\t";
+
+    return tab;
+}
+
 int main()
 {
 
